9ebc2aa58b556f41adf0e9031bef0bcd2bb85089
Tushar Gangani <tushar1710vg@gmail.com>
2022-02-01 13:37:53 +0530

    uploaded assignment-3

---
+++ Assignment-3/qus1/Program.cs
@@ -0,0 +1,24 @@
+﻿int numerator, denom, whole_no;
+float float_div;
+
+Console.WriteLine("Enter Numerator : ");
+numerator = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Denominator : ");
+denom = Convert.ToInt32(Console.ReadLine());
+
+int int_div = numerator / denom;
+int int_rem = numerator % denom;
+
+Console.WriteLine("Integer division result = {0} with a remainder {1}", int_div, int_rem);
+float_div = (float)numerator / denom;
+Console.WriteLine("Floating point division result = {0}", float_div);
+
+if (numerator >= denom){
+    whole_no = numerator / denom;
+    numerator = numerator % denom;
+    Console.WriteLine("The result as a mixed fraction is {0} {1}/{2}", whole_no, numerator, denom);
+}
+else if (numerator % denom == 0){
+    whole_no = numerator / denom;
+    Console.WriteLine("The result as a mixed fraction is {0}", whole_no);
+}
---
+++ Assignment-3/qus2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Part 1:
+string str;
+
+Console.WriteLine("Enter String:");
+str = Console.ReadLine();
+
+Console.WriteLine($"String : {str}\nLength of String: {str.Length}");
+
+// Part:2
+string sentance;
+Console.WriteLine("Enter Sentance:");
+str = Console.ReadLine();
+
+if(str.EndsWith('.')){
+    Console.WriteLine("This is declarative sentance");
+}
+else if(str.EndsWith('?')){
+    Console.WriteLine("This is interrogatory sentance");
+}
+else if(str.EndsWith('!')){
+    Console.WriteLine("This is exclamation sentance");
+}
+else {
+    Console.WriteLine("This is unknown sentance");
+}
+
+// Part:3
+string fullName;
+Console.WriteLine("Enter your Name:");
+fullName = Console.ReadLine();
+
+String[] name = fullName.Split(' ');
+
+if(name.Length==1){
+    Console.WriteLine($"Your Name: {name[0]}");
+}
+else {
+    Console.WriteLine($"Your Name: {name[1]}, {name[0]}");
+}
+
