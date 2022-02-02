59c2dc48c16700ccc7c4a2a7837652b59c25348f
Tushar Gangani <tushar1710vg@gmail.com>
2022-02-02 12:40:41 +0530

    qus3 bug solved

--- Assignment-3/qus3/Program.cs
+++ Assignment-3/qus3/Program.cs
@@ -1,41 +1,29 @@
-﻿public enum Days{
-    None = 0b_0000_0000, // 0
-    Monday = 0b_0000_0001, // 1
-    Tuesday = 0b_0000_0010, // 2
-    Wednesday = 0b_0000_0100, // 4
-    Thursday = 0b_0000_1000, // 8
-    Friday = 0b_0001_0000, // 16
-    Saturday = 0b_0010_0000, // 32
-    Sunday = 0b_0100_0000, // 64
-    Weekend = Saturday | Sunday
-}
+﻿namespace Assignment_3 {
+    [Flags]
+    public enum Days {
+        None = 0b_0000_0000, // 0
+        Monday = 0b_0000_0001, // 1
+        Tuesday = 0b_0000_0010, // 2
+        Wednesday = 0b_0000_0100, // 4
+        Thursday = 0b_0000_1000, // 8
+        Friday = 0b_0001_0000, // 16
+        Saturday = 0b_0010_0000, // 32
+        Sunday = 0b_0100_0000, // 64
+        Weekend = Saturday | Sunday
+    }
+    class Program {
+        static void Main(string[] args) {
+            Days meetingDays = Days.Tuesday | Days.Thursday;
+            Console.WriteLine(meetingDays);
 
-public class FlagsEnumExample{
-    public static void Main(){
-        Days meetingDays = Days.Monday | Days.Wednesday | Days.Friday;
+            Days workingFromHomeDays = Days.Thursday | Days.Saturday;
+            Console.WriteLine($"Join a meeting by phone on {meetingDays & workingFromHomeDays}");
 
-        Console.WriteLine(meetingDays);
+            bool isMeetingOnTuesday = (meetingDays & Days.Tuesday) == Days.Tuesday;
+            Console.WriteLine($"Is there a meeting on Tuesday: {isMeetingOnTuesday}");
 
-        // Output:
-        // Monday, Wednesday, Friday
-
-        Days workingFromHomeDays = Days.Thursday | Days.Friday;
-
-        Console.WriteLine($"Join a meeting by phone on {meetingDays & workingFromHomeDays}");
-
-        // Output:
-        // Join a meeting by phone on Friday
-
-        bool isMeetingOnTuesday = (meetingDays & Days.Tuesday) == Days.Tuesday;
-
-        Console.WriteLine($"Is there a meeting on Tuesday: {isMeetingOnTuesday}");
-
-        // Output:
-        // Is there a meeting on Tuesday: False
-
-        var a = (Days)37;
-        Console.WriteLine(a);
-        // Output:
-        // Monday, Wednesday, Saturday
+            var a = (Days)93;
+            Console.WriteLine(a);
+        }
     }
 }
